6600fe886fc36f4ab176e09366927dd828b54f6f
Sophie Orta <sophie@travis-ci.org>
2020-06-22 15:16:26 -0400

    Moved years to service

--- app/components/layouts/main.js
+++ app/components/layouts/main.js
@@ -1,5 +1,6 @@
 import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
 
 export default class LayoutsMainComponent extends Component {
-  year = new Date().getFullYear();
+  @service() years;
 }
---
+++ tests/unit/services/years-test.js
@@ -0,0 +1,17 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+
+module('Unit | Service | years', function(hooks) {
+  setupTest(hooks);
+
+  // Replace this with your real tests.
+  test('basic functionality', function(assert) {
+    const currentYear = new Date().getFullYear();
+    const devStart = 2011;
+    let service = this.owner.lookup('service:years');
+    assert.ok(service, 'service exists');
+
+    assert.equal(service.current, currentYear, 'current year is correct');
+    assert.equal(service.devSpan, (currentYear - devStart), 'dev year span is correct');
+  });
+});
